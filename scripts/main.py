3ec4fc2f024810ad253df13ae0d02eb601eda7a0
sy-md <mdorsetti733@gmail.com>
2023-03-07 05:07:56 -0700

    scans users playlist - finds total amount - cleans data | next send data to db make new script

--- scripts/main.py
+++ scripts/main.py
@@ -1,15 +1,10 @@
 from youtube_api import get_raw_youtube_data
 
-
 def main():
     ans = input("do you wanna update the database y/n")
-
     if ans == "y":
-        get_raw_youtube_data.get_update()
-        print("updated db with new videos")
+        get_raw_youtube_data.getplaylist()
     else:
         exit()
-
-
 if __name__ == "__main__":
     main()
