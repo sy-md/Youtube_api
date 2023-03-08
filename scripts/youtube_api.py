e46e7ac3eca12d0908564f2f47de32571d836d0c
sy-md <mdorsetti733@gmail.com>
2023-03-08 10:31:27 -0700

    cleaned up the app just need to be able to only add what new

--- scripts/youtube_api.py
+++ scripts/youtube_api.py
@@ -1,8 +1,8 @@
 from googleapiclient.discovery import build
 import json
 import os
+import time
 import logging as lg 
-from mongo_db import pull_data_from_db, push_new_videos
 from pymongo import MongoClient
 from cleaning_data import clean_raw_data
 from dotenv import load_dotenv, find_dotenv
@@ -43,22 +43,24 @@ class get_raw_youtube_data():
         my_youtube = get_raw_youtube_data.send_request() # helper function to get youtube api
         
 
-        request = my_youtube.playlistItems().list(
+        get_amt = my_youtube.playlistItems().list(
             part="snippet,id",
             maxResults=get_raw_youtube_data.amt,
             playlistId=playlist,
         )
 
-        response = request.execute()
+        response = get_amt.execute()
         new_amt = response["pageInfo"]["totalResults"]
-        my_lg.info("{} videos left in Playlist".format(new_amt))
         get_raw_youtube_data.amt = new_amt
+        my_lg.info("{} videos left in Playlist".format(new_amt))
+        time.sleep(3)
 
         request = my_youtube.playlistItems().list(
             part="snippet,id",
             maxResults=get_raw_youtube_data.amt,
             playlistId=playlist,
         )
+
         my_lg.info("grabbed the youtube playlist ")
         response = request.execute()
         clean_raw_data(response)
@@ -68,54 +70,3 @@ class get_raw_youtube_data():
         my_lg.info("Storing user data")
 
         
-###############################################
-
-    def get_update():
-        get_raw_youtube_data.getplaylist()  # get the api platlist / creates data.json
-
-        my_lg.info("exiting")
-        exit()
-
-
-
-
-
-        get_sv_data = pull_data_from_db(mydb) # get datbase playlist
-        my_lg.info("pulling data from my database")
-
-
-        with open(file, "r") as read:  # opeing the youtube api json / data.json
-            data = json.load(read)
-
-            items = data["items"] # updated videos from youtube api
-            tmp = []
-            tmp = []
-            cntt = 0
-            vid_amt = 1
-
-            while cntt < len(items):
-                channel = items[cntt]["snippet"]["videoOwnerChannelTitle"]
-                song_title = items[cntt]["snippet"]["title"]
-                if song_title not in seen:
-                    vid = {channel: song_title}
-                    tmp.append(vid)
-                cntt += 1
-
-            get_raw_youtube_data.amt = (cntt // 2)
-            clean_for_percetage()
-            print("found {} new song".format(len(tmp))) # found 10 songs
-            for x in tmp:
-                print("{}:{} send to db".format(vid_amt, x))  # 20-40
-                push_new_videos(mydb)
-                vid_amt += 1
-                print(vid_amt)
-                print("{} > {}".format(get_raw_youtube_data.amt, cntt))
-                if get_raw_youtube_data.amt > cntt:
-                    exit()
-                else:
-                    print("cycleing")
-                    get_raw_youtube_data.getplaylist()
-                    get_raw_youtube_data.get_update()
-
-
-
